f461d1d00924e03235daee9d8af495fee2f85b5c
root <root@DESKTOP-KB1MGH5.localdomain>
2021-09-14 02:49:11 -0700

    test

--- src/Http/Controllers/Controller.php
+++ src/Http/Controllers/Controller.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace JohnDoe\BlogPackage\Http\Controllers;
+namespace Namviet\Account\Http\Controllers;
 
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
